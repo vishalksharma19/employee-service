97a655c8728174e7c6baa8b108dcee45a7eb3bb6
Vishal Sharma <vishal_sharma11@Dellteam.com>
2021-02-06 17:32:49 +0530

    ApiOperation detail added

--- src/main/java/com/societe/generale/employee/controller/EmployeeController.java
+++ src/main/java/com/societe/generale/employee/controller/EmployeeController.java
@@ -3,6 +3,8 @@ package com.societe.generale.employee.controller;
 import com.societe.generale.employee.dto.EmployeeDTO;
 import com.societe.generale.employee.repository.entity.Employee;
 import com.societe.generale.employee.service.EmployeeService;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -11,17 +13,21 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value="/employee-service/v1/")
+@Slf4j
 public class EmployeeController {
 
     @Autowired
     EmployeeService employeeService;
 
+    @ApiOperation(value = "Create new employee data")
     @CrossOrigin(origins = "http://localhost:4200")
     @PostMapping("employee")
     public Employee createEmployee(@RequestBody @Validated EmployeeDTO employeeDTO){
+        log.info("createEmployee Method Request Object {}",employeeDTO);
          return employeeService.createNewEmployee(employeeDTO);
     }
 
+    @ApiOperation(value = "Publishes all available employee")
     @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("employee")
     public List<Employee> getAllEmployee(){
